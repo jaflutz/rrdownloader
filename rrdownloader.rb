b4325ef4151254a71b05f16731654a3312290682
João Lutz <jaflutz@gmail.com>
2013-10-22 01:41:19 -0200

    Some refactoring

--- rrdownloader.rb
+++ rrdownloader.rb
@@ -1,17 +1,20 @@
 require 'mechanize'
+require 'logger'
 
 agent = Mechanize.new
+logger = Logger.new(STDOUT)
+
 
 page = agent.get('http://rubyrogues.com/episode-guide/')
 
 agent.pluggable_parser.default = Mechanize::Download
 
-page.links_with(:text => /\d\d\d RR/).each do |link|
-  puts "Testing " + link.text + "..."
-  if !Dir.entries(ENV['RROGUES_HOME']).include? (link.text + '.mp3')
-    puts "Downloading " + link.text + "..."
+page.links_with(text: /\d\d\d RR/).each do |link|
+  logger.info 'Testing ' + link.text +  '...'
+  unless Dir.entries(ENV['RROGUES_HOME']).include? (link.text + '.mp3')
+    logger.info 'Downloading ' + link.text + '...'
     following_page = link.click
-    link_href = following_page.links_with(:text => 'Download')[0].href
+    link_href = following_page.links_with(text: 'Download')[0].href
     agent.get(link_href).save(ENV['RROGUES_HOME'] + link.text + '.mp3')
   end
 end
